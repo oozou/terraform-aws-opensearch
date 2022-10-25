c2254be7ec99ffac5685834550967c217750ac73
sedthawut tipkanpirome <40098197+xshot9011@users.noreply.github.com>
2022-10-25 16:45:39 +0700

    (update): security group

--- sg.tf
+++ sg.tf
@@ -1,3 +1,28 @@
+resource "aws_security_group" "client" {
+  count       = var.is_create_security_group ? 1 : 0
+  name        = format("%s-%s-opensearch-sg", local.prefix, var.cluster_name)
+  description = "Security group for allow internal VPC interact with OpenSearch"
+  vpc_id      = data.aws_vpc.this[0].id
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  egress {
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    security_groups = [aws_security_group.this.id]
+  }
+
+  tags = merge(
+    {
+      "Name" = format("%s-%s-opensearch-sg", local.prefix, var.cluster_name)
+    },
+    local.tags
+  )
+}
+
 resource "aws_security_group" "this" {
   count       = var.is_create_security_group ? 1 : 0
   name        = format("%s-%s-opensearch-sg", local.prefix, var.cluster_name)
@@ -5,12 +30,24 @@ resource "aws_security_group" "this" {
   vpc_id      = data.aws_vpc.this[0].id
 
   ingress {
-    from_port = 443
-    to_port   = 443
-    protocol  = "tcp"
-
-    cidr_blocks = concat([data.aws_vpc.this[0].cidr_block], var.additional_allow_cidr)
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    security_groups = [aws_security_group.client.id]
   }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
   tags = merge(
     {
       "Name" = format("%s-%s-opensearch-sg", local.prefix, var.cluster_name)
