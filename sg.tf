6e9417f5322e300832e37be509ef2107e946add0
sedthawut tipkanpirome <40098197+xshot9011@users.noreply.github.com>
2022-10-25 16:45:56 +0700

    (update): security group

--- sg.tf
+++ sg.tf
@@ -1,6 +1,6 @@
 resource "aws_security_group" "client" {
   count       = var.is_create_security_group ? 1 : 0
-  name        = format("%s-%s-opensearch-sg", local.prefix, var.cluster_name)
+  name        = format("%s-%s-opensearch-client-sg", local.prefix, var.cluster_name)
   description = "Security group for allow internal VPC interact with OpenSearch"
   vpc_id      = data.aws_vpc.this[0].id
 
@@ -17,7 +17,7 @@ resource "aws_security_group" "client" {
 
   tags = merge(
     {
-      "Name" = format("%s-%s-opensearch-sg", local.prefix, var.cluster_name)
+      "Name" = format("%s-%s-opensearch-client-sg", local.prefix, var.cluster_name)
     },
     local.tags
   )
