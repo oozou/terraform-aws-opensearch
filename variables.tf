e7a5fc64d167dcc1ad3d358ef523401a2d4b123c
Brian - oozou <98243528+lycbrian@users.noreply.github.com>
2023-06-09 20:04:50 +0700

    chore: update master instance type condition

--- variables.tf
+++ variables.tf
@@ -61,7 +61,7 @@ variable "master_instance_type" {
   default     = "c6g.large.search"
 
   validation {
-    condition     = can(regex("^[r6g|c6g|m6g|r6gd]", var.master_instance_type))
+    condition     = can(regex("^[t3|i3|c5|m5|r5|r6g|c6g|m6g|r6gd]", var.hot_instance_type))
     error_message = "The EC2 master_instance_type must provide a SSD or NVMe-based local storage."
   }
 }
